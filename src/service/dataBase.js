97357b4f8831347efd976298c7a5e6c0f7e64c0d
Eugene <90246528+EvgeshaSuperCute@users.noreply.github.com>
2021-11-29 02:11:25 +0100

    Update dataBase.js

--- src/service/dataBase.js
+++ src/service/dataBase.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import firebase from "firebase/compat/app";
+import firebase from "firebase/compat";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
